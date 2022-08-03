83220f0abd407abd2d27246dcc531882e9b9b20e
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-08-03 15:37:59 +0100

    more

--- run.sh
+++ run.sh
@@ -4,15 +4,30 @@ set +x
 export USE_LOCAL_AMS=true
 export REGISTRY_ENABLE_MULTITENANCY=true
 
-# Delete repos if they exists
+
+export AUTH_ENABLED=true
+export KEYCLOAK_URL='https://auth.apicur.io/auth'
+export KEYCLOAK_REALM='operate-first-apicurio'
+export TENANT_MANAGER_AUTH_ENABLED='true'
+export TENANT_MANAGER_AUTH_SERVER_URL='https://auth.apicur.io/auth'
+export TENANT_MANAGER_AUTH_SERVER_REALM='operate-first-apicurio'
+export TENANT_MANAGER_AUTH_CLIENT_ID='sr-tenant-manager'
+export TENANT_MANAGER_AUTH_SECRET="${TENANT_MANAGER_CLIENT_SECRET}"
+export ORGANIZATION_ID_CLAIM="organization_id"
+
+export REGISTRY_QUOTA_PLANS_CONFIG_FILE="${PWD}/srs-fleet-manager/dist/docker-compose/config/quota-plans.yaml"
+export REGISTRY_DEPLOYMENTS_CONFIG_FILE="${PWD}/registry-deployments.yaml"
+
+
+# # Delete repos if they exists
 rm -rf apicurio-registry
 rm -rf srs-fleet-manager
 
-# Clone the repos
+# # Clone the repos
 git clone https://github.com/Apicurio/apicurio-registry.git --depth 1 --single-branch --branch feat/hackathon
 git clone https://github.com/bf2fc6cc711aee1a0c2a/srs-fleet-manager.git --depth 1 --single-branch --branch feat/hackathon
 
-# Build the repos locally
+# # Build the repos locally
 (
   cd apicurio-registry && \
   mvn clean install -DskipTests
@@ -27,13 +42,14 @@ git clone https://github.com/bf2fc6cc711aee1a0c2a/srs-fleet-manager.git --depth 
   yarn install
 )
 
-yarn --cwd apicurio-registry/ui install
-
 # Run the 3 services
+export KEYCLOAK_API_CLIENT_ID='sr-api'
 mvn compile exec:java -Dexec.mainClass="io.apicurio.registry.RegistryQuarkusMain" -f apicurio-registry/app/pom.xml &
 
+export KEYCLOAK_API_CLIENT_ID='sr-tenant-manager'
 mvn compile exec:java -Dexec.mainClass="io.apicurio.multitenant.api.TenantManagerQuarkusMain" -f apicurio-registry/multitenancy/tenant-manager-api/pom.xml &
 
+export KEYCLOAK_API_CLIENT_ID='sr-fleet-manager'
 mvn compile exec:java -Dexec.mainClass="org.bf2.srs.fleetmanager.FleetManagerQuarkusMain" -f srs-fleet-manager/core/pom.xml &
 
 yarn --cwd apicurio-registry/ui start &
