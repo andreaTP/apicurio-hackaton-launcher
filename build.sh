6f19076d0c66199ea95fd0f62f346e3db0ba2b83
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-08-04 11:39:47 +0100

    remove a duplicate build

--- build.sh
+++ build.sh
@@ -12,15 +12,10 @@ TARGET="${1:-${SCRIPT_DIR}/target}"
 ) &
 PID_JOB1=$!
 (
-  cd ${TARGET}/apicurio-registry && \
-  mvn clean install -DskipTests
-) &
-PID_JOB2=$!
-(
   cd ${TARGET}/apicurio-registry/ui && \
   ./init-dev.sh && \
   yarn install
 ) &
-PID_JOB3=$!
+PID_JOB2=$!
 
-wait $PID_JOB1 $PID_JOB2 $PID_JOB3
+wait $PID_JOB1 $PID_JOB2
